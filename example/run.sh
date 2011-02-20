f7bbf8d3edee67190ae9234ff9de81a7f331b7b1
Julien Palard <julien@palard.fr>
2011-02-20 20:48:19 +0100

    Adding usage in example/run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ -z "$1" ]
+then
+    echo 'Usage: ' $0 'source.mixal'
+    exit 1
+fi
 python ../assembler/main.py "$1" "/tmp/$1.p" > /dev/null
 python ../vm/main.py "/tmp/$1.p"
 cat printer.out
